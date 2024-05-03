305911f69d343f537627aca80cb06f37fe39ed98
yhyim0627 <115547566+yhyim0627@users.noreply.github.com>
2024-05-03 16:10:48 +0900

    Update Greedy.py

--- CodingTest/Greedy/Greedy.py
+++ CodingTest/Greedy/Greedy.py
@@ -1,1 +1,19 @@
+import sys
 
+n, k = sys.stdin.readline().split()
+n, k = int(n), int(k)
+cnt = 0
+
+while True:
+  if (n == 1):
+    break
+
+  if (n % k == 0):
+    n = n // k
+    cnt += 1
+
+  else:
+    n = n - 1
+    cnt += 1
+
+print(cnt)
